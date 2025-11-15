6ce710f36f294e114836dea4b5a8ab8be98c118b
DmitryOO <dimkut@mail.ru>
2025-11-15 16:59:30 +0400

    4

--- js/preview.js
+++ js/preview.js
@@ -4,22 +4,17 @@ const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 const fileChoser = document.querySelector('#upload-file');
 const upLoadPreview = document.querySelector('.img-upload__preview img');
 const upLoadPreviewEffects = document.querySelectorAll('.effects__preview');
-const FReader = new FileReader();
+
 const loadImageFile = () => {
   const file = fileChoser.files[0];
   const fileName = file.name.toLowerCase();
   const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
   if (matches) {
-    FReader.readAsDataURL(file);
 
-    FReader.onload = function (evt) {
-
-      upLoadPreview.src = evt.target.result;
-      upLoadPreviewEffects.forEach((item) => {
-        item.style.backgroundImage = `url(${evt.target.result})`;
-      });
-
-    };
+    upLoadPreview.src = URL.createObjectURL(file);
+    upLoadPreviewEffects.forEach((item) => {
+      item.style.backgroundImage = `url(${URL.createObjectURL(file)})`;
+    });
   } else {
     onFormCloseClick();
   }
